c2e0669a3bcb6b156f4ea3c150fb8f4a15fd6050
Sulaiman Hawibowo <74175541+Bowaaw18@users.noreply.github.com>
2020-11-29 16:52:04 +0700

    Update Latihan_Soal_5.R

--- Latihan_Soal_5.R
+++ Latihan_Soal_5.R
@@ -1,14 +1,17 @@
-tugas_5 = read.csv("dataset_superstore_simple.csv")
+#Answered by Sulaiman Hawibowo
+#From Dataset Superstore Simple
+
+case_5 = read.csv("dataset_superstore_simple.csv")
 library(dplyr)
 
-tugas_5$order_date = as.Date(tugas_5$order_date)
-tugas_5$order_year = as.Date(cut(tugas_5$order_date, breaks = "year"))
-NROW(tugas_5)
-yearly_sales = tugas_5%>%group_by(order_year)%>%summarise(total_sales = sum(sales),
+case_5$order_date = as.Date(case_5$order_date)
+case_5$order_year = as.Date(cut(case_5$order_date, breaks = "year"))
+NROW(case_5)
+yearly_sales = case_5%>%group_by(order_year)%>%summarise(total_sales = sum(sales),
                                                       total_profit = sum(profit),
                                                       jumlah_customers = n())
-urutan_profit_terbesar = yearly_sales[order(-yearly_sales$total_profit),]
-jawaban_nomor_5 = head(urutan_profit_terbesar, 1)
+desc_profit = yearly_sales[order(-yearly_sales$total_profit),]
+result_No_5 = head(desc_profit, 1)
 
 
 
