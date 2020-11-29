ac75c69574eb01bdc491686323e20b111f26ef4d
Sulaiman Hawibowo <74175541+Bowaaw18@users.noreply.github.com>
2020-11-29 15:29:55 +0700

    Update Case_No_1.R

--- Case_No_1.R
+++ Case_No_1.R
@@ -1,11 +1,11 @@
 #Answered by Sulaiman Hawibowo
 #From Dataset Superstore Simple
 
-tugas_1 = read.csv('dataset_superstore_simple.csv')
+case_1 = read.csv('dataset_superstore_simple.csv')
 
 library(dplyr)
-step1        = select(tugas_1, c(customer_id,sales))
-distinct = distinct(tugas_1, customer_id)
-step2        = tugas_1%>%group_by(customer_id)%>%summarise(max_sales = sum(sales))
-desc_data        = step2[order(-step2$max_sales),]
-jawaban_tugas_1 = head(urutan1, 1)
+step1         = select(case_1, c(customer_id,sales))
+distinct      = distinct(case_1, customer_id)
+step2         = case_1%>%group_by(customer_id)%>%summarise(max_sales = sum(sales))
+desc_data     = step2[order(-step2$max_sales),]
+result_case_1 = head(desc_data, 1)
