84b64569abffb8c0ecdda9a7ca778d1a08c3c516
Sulaiman Hawibowo <74175541+Bowaaw18@users.noreply.github.com>
2020-11-29 15:28:15 +0700

    Update Case_No_2.R

--- Case_No_2.R
+++ Case_No_2.R
@@ -1,9 +1,9 @@
 #Answered by Sulaiman Hawibowo
 #From Dataset Superstore Simple
 
-tugas_2 = read.csv("dataset_superstore_simple.csv")
+case_2 = read.csv("dataset_superstore_simple.csv")
 library(dplyr)
-jawaban_nomor_2 = tugas_2%>%
+result_no_2 = case_2%>%
                   group_by(category,sub_category)%>%
                   summarise(total_profit = sum(profit))%>%
                   filter(category == ("Office Supplies"))
