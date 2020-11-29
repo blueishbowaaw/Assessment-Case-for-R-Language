2f753bb3990c33b8681ec98d17f69e7927cf6976
Sulaiman Hawibowo <74175541+Bowaaw18@users.noreply.github.com>
2020-11-29 16:58:02 +0700

    Update Latihan_Soal_6.R

--- Latihan_Soal_6.R
+++ Latihan_Soal_6.R
@@ -1,13 +1,17 @@
-tugas_6 = read.csv("dataset_superstore_simple.csv")
+#Answered by Sulaiman Hawibowo
+#From Dataset Superstore Simple
+
+
+case_6 = read.csv("dataset_superstore_simple.csv")
 library(dplyr)
 library(ggplot2)
 
-tugas_6$order_date = as.Date(tugas_6$order_date)
-tugas_6$order_year = as.Date(cut(tugas_6$order_date, breaks = "year"))
-hasil_6a = tugas_6%>%filter(order_year == (c("2014-01-01","2015-01-01")))
-hasil_6b = select(hasil_6a, order_date,order_year,sales,profit)
-urutan_tahun_terbaru = hasil_6b[order(hasil_6b$order_date),]
-jawaban_nomor_6 = ggplot(urutan_tahun_terbaru, aes(x = sales, y = profit)) + 
-  geom_point(aes(color = order_date))+
-  labs(title = 'Scatter Plot Sales Vs Profit 2014-2015')
-jawaban_nomor_6
+case_6$order_date = as.Date(case_6$order_date)
+case_6$order_year = as.Date(cut(case_6$order_date, breaks = "year"))
+step1             = case_6%>%filter(order_year == (c("2014-01-01","2015-01-01")))
+step2             = select(step1, order_date,order_year,sales,profit)
+asc_newly_year    = step2[order(step2$order_date),]
+result_No_6       = ggplot(asc_newly_year, aes(x = sales, y = profit)) + 
+                  geom_point(aes(color = order_date))+
+                  labs(title = 'Scatter Plot Sales Vs Profit 2014-2015')
+result_No_6
