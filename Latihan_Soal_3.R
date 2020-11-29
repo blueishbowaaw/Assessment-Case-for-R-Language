79c87f462855b03fbd5d46c77280f90117b23578
Sulaiman Hawibowo <74175541+Bowaaw18@users.noreply.github.com>
2020-11-29 15:22:49 +0700

    Update Latihan_Soal_3.R

--- Latihan_Soal_3.R
+++ Latihan_Soal_3.R
@@ -1,4 +1,4 @@
 tugas_3 = read.csv("dataset_superstore_simple.csv")
 library(dplyr)
-Nomor_3a = tugas_3%>%filter(profit<0)%>%select(c(order_id, profit))
-jawaban_nomor_3 = NROW(Nomor_3a)
+Step1 = tugas_3%>%filter(profit<0)%>%select(c(order_id, profit))
+jawaban_nomor_3 = NROW(Step1)
