993fed4781c53f0a154aff99307499c9a224ee84
Sulaiman Hawibowo <74175541+blueishbowaaw@users.noreply.github.com>
2020-12-15 17:17:34 +0700

    Update Case_No_2.R

--- Case_No_2.R
+++ Case_No_2.R
@@ -1,6 +1,9 @@
 #Answered by Sulaiman Hawibowo
 #From Dataset Superstore Simple
 
+Case2 = Sub-category apa saja yang ada di dalam category ‘Office Supplies’, dan
+masing-masing berapa banyak total profitnya?
+
 case_2 = read.csv("dataset_superstore_simple.csv")
 library(dplyr)
 result_no_2 = case_2%>%
