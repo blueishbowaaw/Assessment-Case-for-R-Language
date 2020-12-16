bcd2fb4bf56c59c33fbe1640675f52160426cced
Sulaiman Hawibowo <74175541+blueishbowaaw@users.noreply.github.com>
2020-12-16 13:58:05 +0700

    Update Case_No_3.R

--- Case_No_3.R
+++ Case_No_3.R
@@ -1,6 +1,8 @@
 #Answered by Sulaiman Hawibowo
 #From Dataset Superstore Simple
 
+#Case_3: Berapa banyak order yang menghasilkan profit negatif (rugi)?
+
 case_3 = read.csv("dataset_superstore_simple.csv")
 library(dplyr)
 step1 = tugas_3%>%filter(profit<0)%>%select(c(order_id, profit))
