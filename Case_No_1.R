dcf48f5648a614feeb5657324f8966737a7fae5b
Sulaiman Hawibowo <74175541+blueishbowaaw@users.noreply.github.com>
2020-12-30 17:02:30 +0700

    Update Case_No_1.R

--- Case_No_1.R
+++ Case_No_1.R
@@ -8,4 +8,4 @@ step1         <- select(case_1, c(customer_id,sales))
 distinct      <- distinct(case_1, customer_id)
 step2         <- case_1%>%group_by(customer_id)%>%summarise(max_sales = sum(sales))
 desc_data     <- step2[order(-step2$max_sales),]
-result_case_1 = head(desc_data, 1)
+result_case_1 <- head(desc_data, 1)
